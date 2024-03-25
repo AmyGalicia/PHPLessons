47be8399d5184f57d1de5bfa3edb487fbdf259ba
AmyGalicia <141743985+AmyGalicia@users.noreply.github.com>
2024-03-25 08:06:56 -0500

    day3

---
+++ datatypes.php/operators.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+<?php
+    
+    //Arithmetic Operators
+    echo 5+5;
+
+?>
+
+<?php
+
+//Assignment Operators
+$x = 100;
+$x +=20;
+
+?>
+
+<?php
+
+//Comparison Operators 
+$x = 10;
+$y = 10;
+
+if ($x >= $y) {
+    echo "True!";
+}
+else {
+    echo "False!";
+}
+
+?>
+
+<?php
+
+//Increment/Decrement Operators
+$x =10;
+echo $x--;
+echo $x;
+
+?>
+
+<?php
+
+//Logical Operators
+$x =10;
+$y =20;
+
+if ($x == $y || 1 == 1) {
+    echo "True"
+}
+?>
+</body>
+</html>
