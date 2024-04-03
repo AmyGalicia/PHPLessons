643d95de16d80d1eba986d2d772ce3e0d1345594
AmyGalicia <141743985+AmyGalicia@users.noreply.github.com>
2024-04-03 07:29:18 -0500

    day6

--- functions.php
+++ functions.php
@@ -5,13 +5,28 @@
     <meta name="viewport" content="width=, initial-scale=1.0">
     <title>Document</title>
 </head>
-<body>
-
+<body> 
 <?php
     
     echo str_replace("Amy", "Claudia", "Hi Amy");
 
-    ?>
+?>
+<br>
+<?php
 
+$x = 100;
+
+function newCalc($x) {
+    $newnr = $x * 0.75;
+    echo "Here is 75% of what you wrote: ".$newnr;
+}
+
+$x = 100;
+newCalc($x);
+echo "<br>";
+$a = 10;
+newCalc($a);
+
+?>
 </body>
 </html>
