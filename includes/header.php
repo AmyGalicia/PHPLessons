3ab1e3356042a41dc2f24724e5d268b2cb245977
AmyGalicia <141743985+AmyGalicia@users.noreply.github.com>
2024-04-04 08:30:01 -0500

    day6

---
+++ includes/header.php
@@ -0,0 +1,21 @@
+<nav class="navbar navbar-expand-lg bg-body-tertiary">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">Navbar</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+      <div class="navbar-nav">
+        <a class="nav-link active" aria-current="page" href="calculator.php">calculator</a>
+        <a class="nav-link" href="calender.php">calender</a>
+        <a class="nav-link" href="conditionals.php">conditionals</a>
+        <a class="nav-link" href="datatypes.php">datatypes</a>
+        <a class="nav-link" href="functions.php">functions</a>
+        <a class="nav-link" href="index.php">index</a>
+        <a class="nav-link" href="loops.php">loops</a>
+        <a class="nav-link" href="operators.php">operators</a>
+        <a class="nav-link disabled" aria-disabled="true">Disabled</a>
+      </div>
+    </div>
+  </div>
+</nav>
