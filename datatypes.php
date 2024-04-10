273c00728037eef73b79e5cdbb0109da10c7f49a
AmyGalicia <141743985+AmyGalicia@users.noreply.github.com>
2024-04-10 07:23:32 -0500

    day6

--- datatypes.php
+++ datatypes.php
@@ -1,15 +1,7 @@
 <?php
     include 'includes/header.php';
 ?>
-<!doctype html>
-<html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Bootstrap demo</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-  </head>
- <body>
+
 </html>
 <?php
     //String
@@ -22,12 +14,11 @@
     $name= 7.4564;
 
     //BooLean
-    true = 2
-    false= 0
+   
 
     //Array
-    $names = array("Amy", "Gabriela", "Galicia"),
-    echo $names{'0'};
+    $names = array("Amy", "Gabriela", "Galicia");
+    echo $names['2'];
 
 ?>
 
--- superglobals.php
+++ superglobals.php
@@ -1,4 +1,3 @@
-
 <!doctype html>
 <html lang="en">
   <head>
@@ -6,19 +5,16 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Bootstrap demo</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
- <link rel="stylesheet" href="style.css">
   </head>
-  <body>
-    <?php
-    include 'includes/header.php';
-?>
-<?php
-    
-    echo str_replace("Amy", "Claudia", "Hi Amy");
+  <?php
+
+/*
+$_COOKIE
+$_SESSION
+*/
+
+setcookie("name", "Daniel", time() - 2);
+
+$_SESSION['name'] = "Amy";
 
 ?>
-
-
-
-</body>
-</html>
